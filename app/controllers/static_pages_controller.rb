574b7e07a729b8a8a8a3cf567d3b1694bc6e0740
kosuke <gekkouzann334@gmail.com>
2019-07-18 01:01:04 +0900

    home page edit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,8 @@ class StaticPagesController < ApplicationController
   def home
     Video.delete_all
     @video = Video.new
-    get_data
+    get_data("PooL Live VS")
+    get_data("World Cup Of Pool 2019")
 #    @video = Video.page(params[:page])
     @videos = Video.all
   end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -20,12 +20,12 @@ module StaticPagesHelper
     return client, youtube
   end
 
-  def get_data
+  def get_data(keyword)
     arg_array_save = []
     arg_array_save = ARGV.shift(4)
     opts = Optimist::options do
-      opt :Srch, 'Search term', :type => String, :default => "Bustamante pool"
-      opt :max_results, 'Max results', :type => :int, :default => 5
+      opt :Srch, 'Search term', :type => String, :default => keyword
+      opt :max_results, 'Max results', :type => :int, :default => Settings.lines_per_page
       opt :order, 'order', :type => String, :default => 'date'
       opt :regionCode, 'region', :type => String, :default => 'JP'
     end
@@ -49,16 +49,10 @@ module StaticPagesHelper
         }
       )
       videos = search_response.data.items
-#      for video_index in 0..videos.length - 1 do
-#          Video.create!(title: videos[video_index]["snippet"]["title"],
-#                        description: videos[video_index]["snippet"]["description"],
-#                        url: Settings.jump_url + videos[video_index]["id"]["videoId"],
-#                        thumbnail_img: videos[video_index]["snippet"]["thumbnails"]["default"]["url"])
-#      end
       videos.each_with_index do |input_db_val| 
           Video.create!(title: input_db_val["snippet"]["title"],
                         description: input_db_val["snippet"]["description"],
-                        url: Settings.jump_url + input_db_val["id"]["videoId"],
+                        url: Settings.jump_url.to_s + input_db_val["id"]["videoId"].to_s,
                         thumbnail_img: input_db_val["snippet"]["thumbnails"]["default"]["url"])
       end
     rescue Google::APIClient::TransmissionError => e
