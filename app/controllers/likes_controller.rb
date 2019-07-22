d092701e2b9304dca869fa59fa7d0d0d62c106c6
Kosuke <gekkouzann334@gmail.com>
2019-07-22 11:44:08 +0900

    likes route edit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   before_action :logged_in_user
-
   def create
+    byebug
     @video = Video.find(params[:video_id])
     unless @video.good?(current_user)
       @video.good(current_user)
@@ -15,6 +15,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
+    byebug
     @video = Like.find(params[:id]).video
     if @video.good?(current_user)
       @video.not_bad(current_user)
@@ -26,4 +27,14 @@ class LikesController < ApplicationController
 #    @video = Like.find(params[:id]).video
 #    @video.not_bad(current_user)
   end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+
 end
